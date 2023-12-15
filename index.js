e13cc40ca740435bb5858818d2cc9aa76fae9f73
himaloyash <himaloyashis98@gmail.com>
2023-12-15 12:55:18 +0600

    server set up is going on

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 
+const cors = require("cors")
+
+
+app.use(cors())
+app.use(express.json())
+
 
 
 
