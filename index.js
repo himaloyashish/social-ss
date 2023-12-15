08c95c0ae4b3625f8da4a0cc4fc2f09b9935b002
himaloyash <himaloyashis98@gmail.com>
2023-12-15 12:51:37 +0600

    social server is start

---
+++ index.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.get("/", (req, res)=>{
+    res.send("Hello World")
+})
+
+
+app.listen(port, ()=>{
+    console.log(`Social server is running on Port: ${port}`);
+})
