bdee03ea0a1837ab8d2097c18426854e7b684b3e
himaloyash <himaloyashis98@gmail.com>
2023-12-15 17:34:16 +0600

    another demo update api created

--- index.js
+++ index.js
@@ -38,6 +38,10 @@ async function run() {
         res.send(result);
     })
 
+    // app.get("/", async(req, res)=>{
+    //     const result = await groupCollection.updateOne()
+    // })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
